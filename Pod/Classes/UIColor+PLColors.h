098f8c0a5e491e9d90b0636ebed6d11261d4025d
Michał Laskowski <michal.laskowski@polidea.com>
2015-01-15 20:24:18 +0100

    Library methods implementation. Added comments to interface file.

--- Pod/Classes/UIColor+PLColors.h
+++ Pod/Classes/UIColor+PLColors.h
@@ -2,7 +2,22 @@
 
 @interface UIColor (PLColors)
 
+/**
+*   Creates UIColor object based on hexadecimal string without alpha component.
+*
+*   @param hexString string describing color in format RRGGBB or RGB, where R,G,B are hexadecimal digits describing
+*   color component values. Optionally it can have # symbol at the beginning.
+*
+*/
 + (UIColor *)colorWithHexString:(NSString *)hexString;
+
+/**
+*   Creates UIColor object based on hexadecimal string with alpha component.
+*
+*   @param hexString string describing color in format RRGGBBAA or RGBA, where R,G,B,A are hexadecimal digits describing
+*   color component values. Optionally it can have # symbol at the beginning.
+*
+*/
 + (UIColor *)colorWithHexStringWithAlpha:(NSString *)hexString;
 
 @end
--- Pod/Classes/UIColor+PLColors.m
+++ Pod/Classes/UIColor+PLColors.m
@@ -4,11 +4,72 @@
 @implementation UIColor (PLColors)
 
 + (UIColor *)colorWithHexString:(NSString *)hexString {
-    return nil;
+    if (hexString.length == 3 || (hexString.length == 4 && [hexString characterAtIndex:0] == '#')) {
+        hexString = [hexString stringByAppendingString:@"F"];
+    }
+    else {
+        hexString = [hexString stringByAppendingString:@"FF"];
+    }
+
+    return [UIColor colorWithHexStringWithAlpha:hexString];
 }
 
 + (UIColor *)colorWithHexStringWithAlpha:(NSString *)hexString {
-    return nil;
+    if (hexString.length > 0 && [hexString characterAtIndex:0] == '#') {
+        hexString = [hexString substringFromIndex:1];
+    }
+
+    if (hexString.length != 4 && hexString.length != 8) {
+        return nil;
+    }
+
+    if (hexString.length == 4) {
+        unichar char0 = [hexString characterAtIndex:0];
+        unichar char1 = [hexString characterAtIndex:1];
+        unichar char2 = [hexString characterAtIndex:2];
+        unichar char3 = [hexString characterAtIndex:3];
+        hexString = [NSString stringWithFormat:@"%c%c%c%c%c%c%c%c", char0, char0, char1, char1, char2, char2, char3, char3];
+    }
+
+    NSString *componentPattern = @"[0-9A-Fa-f]{2}";
+    NSString *pattern = [NSString stringWithFormat:@"^(%@)(%@)(%@)(%@)$", componentPattern, componentPattern, componentPattern, componentPattern];
+
+    NSError *error;
+    NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:pattern
+                                                                           options:0
+                                                                             error:&error];
+    NSArray *matches = [regex matchesInString:hexString options:0 range:NSMakeRange(0, hexString.length)];
+    if (matches.count != 1) {
+        return nil;
+    }
+
+    NSTextCheckingResult *result = matches[0];
+    if (result.numberOfRanges != 5) {
+        return nil;
+    }
+
+    CGFloat red = [UIColor normalizedFloatColorComponentFromHexString:[hexString substringWithRange:[result rangeAtIndex:1]]];
+    CGFloat green = [UIColor normalizedFloatColorComponentFromHexString:[hexString substringWithRange:[result rangeAtIndex:2]]];
+    CGFloat blue = [UIColor normalizedFloatColorComponentFromHexString:[hexString substringWithRange:[result rangeAtIndex:3]]];
+    CGFloat alpha = [UIColor normalizedFloatColorComponentFromHexString:[hexString substringWithRange:[result rangeAtIndex:4]]];
+
+    if (red < 0.f || green < 0.f || blue < 0.f || alpha < 0.f) {
+        return nil;
+    }
+
+    return [UIColor colorWithRed:red green:green blue:blue alpha:alpha];
 }
+
++ (CGFloat)normalizedFloatColorComponentFromHexString:(NSString *)hexString {
+    NSScanner *scanner = [NSScanner scannerWithString:[NSString stringWithFormat:@"0x%@", hexString]];
+    float floatValue;
+    BOOL result = [scanner scanHexFloat:&floatValue];
+    if (!result || floatValue > 255.0f) {
+        return -1.0f;
+    }
+
+    return floatValue / 255.0f;
+}
+
 
 @end
