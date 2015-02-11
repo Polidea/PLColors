6501892044b706a3b54e7c818b2625d053b6f696
Michał Laskowski <michal.laskowski@polidea.com>
2015-02-11 15:40:52 +0100

    Implement OS X example.

--- OSX PLColors Example/OSX PLColors Example/AppDelegate.m
+++ OSX PLColors Example/OSX PLColors Example/AppDelegate.m
@@ -7,20 +7,57 @@
 //
 
 #import "AppDelegate.h"
+#import "NSColor+PLColors.h"
 
 @interface AppDelegate ()
 
 @property (weak) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *label;
+
 @end
 
-@implementation AppDelegate
-
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+@implementation AppDelegate {
+    NSTimer *_timer;
+    NSUInteger _step;
 }
 
-- (void)applicationWillTerminate:(NSNotification *)aNotification {
-    // Insert code here to tear down your application
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    _timer = [NSTimer scheduledTimerWithTimeInterval:0.8f target:self selector:@selector(timerEvent) userInfo:nil repeats:YES];
+    self.window.backgroundColor = [NSColor whiteColor];
+    _label.backgroundColor = [NSColor colorWithHexInteger:0x000000];
+}
+
+- (void)timerEvent {
+
+    NSColor *color;
+    switch (_step) {
+        case 0:
+            color = [NSColor colorWithHexString:@"000"];
+            break;
+        case 1:
+            color = [NSColor colorWithHexString:@"FF0000"];
+            break;
+        case 2:
+            color = [NSColor colorWithHexInteger:0x04A0FF];
+            break;
+        case 3:
+            color = [NSColor colorWithHexString:@"00FF00"];
+            break;
+        case 4:
+            color = [NSColor colorWithHexStringWithAlpha:@"#0000007F"];
+            break;
+        case 5:
+            color = [NSColor colorWithHexIntegerWithAlpha:0x000FF7F];
+            break;
+        default:
+            break;
+    }
+    _label.backgroundColor = color;
+    
+    _step++;
+    if (_step == 6) {
+        _step = 0;
+    }
 }
 
 @end
